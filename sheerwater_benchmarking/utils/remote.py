e114dacfb9408a3846cdc1433b4860e0216719ff
Genevieve Flaspohler <geflaspohler@gmail.com>
2024-09-10 13:24:21 -0400

    Updated remote config

--- sheerwater_benchmarking/utils/remote.py
+++ sheerwater_benchmarking/utils/remote.py
@@ -15,15 +15,13 @@ def dask_remote(func):
                 # setup coiled cluster with remote config
                 logger.info("Attaching to coiled cluster with custom configuration")
                 cluster = coiled.Cluster(**kwargs['remote_config'])
-                client = cluster.get_client()
-
-                del kwargs['remote_config']
+                _ = cluster.get_client()
             else:
                 # Just setup a coiled cluster
                 logger.info("Attaching to coiled cluster with default configuration")
                 cluster = coiled.Cluster(name='sheerwater-shared',
                                          n_workers=4, idle_timeout="45 minutes")
-                client = cluster.get_client()
+                _ = cluster.get_client()
         else:
             # Setup a local cluster
             try:
@@ -36,6 +34,9 @@ def dask_remote(func):
         # call the function and return the result
         if 'remote' in kwargs:
             del kwargs['remote']
+
+        if 'remote_config' in kwargs:
+            del kwargs['remote_config']
 
         return func(*args, **kwargs)
     return wrapper
