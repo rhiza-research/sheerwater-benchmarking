56e29f1d111279fd07d53097cf65f472432be1eb
Genevieve Flaspohler <geflaspohler@gmail.com>
2024-09-17 14:40:03 -0700

    Fixed init

--- sheerwater_benchmarking/data/__init__.py
+++ sheerwater_benchmarking/data/__init__.py
@@ -1,3 +1,3 @@
-from .era5 import (single_era5, single_era5_cleaned, era5_raw, era5_rolled, era5_agg,
+from .era5 import (single_era5, single_era5_cleaned, era5_rolled, era5_cds, era5_agg,
                    salient_era5_raw, salient_era5)
 from .masks import (land_sea_mask)
