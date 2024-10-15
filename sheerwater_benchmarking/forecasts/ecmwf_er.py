ab2438c7c1584c7283edf36a7b1b8d98ea8d7585
Genevieve Flaspohler <geflaspohler@gmail.com>
2024-10-15 12:01:00 -0700

    Clean up

--- sheerwater_benchmarking/forecasts/ecmwf_er.py
+++ sheerwater_benchmarking/forecasts/ecmwf_er.py
@@ -278,8 +278,7 @@ def iri_ecmwf(start_time, end_time, variable, forecast_type,
     datasets = []
     for date in target_dates:
         ds = dask.delayed(fn)(
-            date, variable, forecast_type, run_type, grid, verbose,
-            filepath_only=True, retry_null_cache=True)
+            date, variable, forecast_type, run_type, grid, verbose, filepath_only=True)
         datasets.append(ds)
     datasets = dask.compute(*datasets)
     data = [d for d in datasets if d is not None]
