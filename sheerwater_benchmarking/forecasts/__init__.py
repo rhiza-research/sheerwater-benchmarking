2f7896509a3e2d2e15430f042ef89fba1f5aa66a
Genevieve Flaspohler <geflaspohler@gmail.com>
2024-10-23 12:50:07 -0700

    Fixed misspell debias

--- sheerwater_benchmarking/forecasts/__init__.py
+++ sheerwater_benchmarking/forecasts/__init__.py
@@ -1,6 +1,6 @@
 """Forecasting models for the Sheerwater benchmarking project."""
 from .salient import salient
-from .ecmwf_er import ecmwf_agg, ecmwf_ifs_er, ecmwf_ifs_er_debaised
+from .ecmwf_er import ecmwf_agg, ecmwf_ifs_er, ecmwf_ifs_er_debiased
 
 # Use __all__ to define what is part of the public API.
-__all__ = ["ecmwf_agg", "salient", "ecmwf_ifs_er_debaised", "ecmwf_ifs_er"]
+__all__ = ["ecmwf_agg", "salient", "ecmwf_ifs_er_debiased", "ecmwf_ifs_er"]
