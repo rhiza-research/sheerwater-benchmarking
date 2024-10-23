ad31d2c8b38cd4148fd851c06a14cd92d5cbfe91
Genevieve Flaspohler <geflaspohler@gmail.com>
2024-10-23 12:27:48 -0700

    Updated init

--- sheerwater_benchmarking/forecasts/__init__.py
+++ sheerwater_benchmarking/forecasts/__init__.py
@@ -1,6 +1,6 @@
 """Forecasting models for the Sheerwater benchmarking project."""
 from .salient import salient
-from .ecmwf_er import ecmwf_agg
+from .ecmwf_er import ecmwf_agg, ecmwf_er_deb, ecmwf_ifs_er
 
 # Use __all__ to define what is part of the public API.
-__all__ = ["ecmwf_agg", "salient"]
+__all__ = ["ecmwf_agg", "salient", "ecmwf_er_deb", "ecmwf_ifs_er"]
