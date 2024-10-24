4f3ae8ecb8b6b427c98ea70c3813c173e3c1f15e
Genevieve Flaspohler <geflaspohler@gmail.com>
2024-10-24 15:22:16 -0700

    Linting

--- sheerwater_benchmarking/forecasts/__init__.py
+++ sheerwater_benchmarking/forecasts/__init__.py
@@ -4,4 +4,4 @@ from .abc import perpp
 from .ecmwf_er import ecmwf_agg, ecmwf_ifs_er, ecmwf_ifs_er_debiased
 
 # Use __all__ to define what is part of the public API.
-__all__ = ["ecmwf_agg", "salient", "ecmwf_ifs_er_debiased", "ecmwf_ifs_er"]
+__all__ = ["ecmwf_agg", "salient", "ecmwf_ifs_er_debiased", "ecmwf_ifs_er", "perpp"]
--- sheerwater_benchmarking/forecasts/abc.py
+++ sheerwater_benchmarking/forecasts/abc.py
@@ -71,14 +71,14 @@ def perpp(start_time, end_time, variable, lead, prob_type='deterministic',
     lead_id = lead_params.get(lead, None)
     if lead is None:
         raise NotImplementedError(f"Lead {lead} not implemented for perpp.")
-    
+
     # Temp: change when we have all variables
     if variable != 'tmp2m':
         raise NotImplementedError(f"Variable {variable} not implemented for perpp.")
 
     ds = perpp_ecmwf(start_time, end_time, variable, lead=lead_id, grid=grid)
     if prob_type != 'deterministic':
-        raise NotImplementedError(f"Probabilistic forecast not implemented for perpp.")
+        raise NotImplementedError("Probabilistic forecast not implemented for perpp.")
     ds = ds.assign_attrs(prob_type="deterministic")
 
     ds = ds.rename({'start_date': 'time'})
