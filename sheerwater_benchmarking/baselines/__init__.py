b16b71e8ac511b83c46df1b67fa801df12304b5c
Genevieve Flaspohler <geflaspohler@gmail.com>
2024-10-29 14:37:39 -0700

    Moved climatology functions

--- sheerwater_benchmarking/baselines/__init__.py
+++ sheerwater_benchmarking/baselines/__init__.py
@@ -2,4 +2,6 @@
 from .climatology import climatology_rolling, climatology_2015, climatology_plus_trend_2015
 
 # Use __all__ to define what is part of the public API.
-__all__ = ["climatology_rolling", "climatology_2015", "climatology_plus_trend_2015"]
+__all__ = ["climatology_rolling", "climatology_2015", "climatology_plus_trend_2015",
+           'climatology', 'climatology_standard_30yr', 'climatology_agg',
+           'climatology_rolling', 'climatology_rolling_agg', 'climatology_trend']
