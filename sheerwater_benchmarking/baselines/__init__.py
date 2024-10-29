c764266f2971327936465807c4538a3da545906d
Genevieve Flaspohler <geflaspohler@gmail.com>
2024-10-29 14:38:36 -0700

    Imports

--- sheerwater_benchmarking/baselines/__init__.py
+++ sheerwater_benchmarking/baselines/__init__.py
@@ -1,5 +1,7 @@
 """Baseline models for the Sheerwater benchmarking project."""
-from .climatology import climatology_rolling, climatology_2015, climatology_plus_trend_2015
+from .climatology import climatology_rolling, climatology_2015, climatology_plus_trend_2015, \
+    climatology, climatology_standard_30yr, climatology_agg,  \
+    climatology_rolling, climatology_rolling_agg, climatology_trend
 
 # Use __all__ to define what is part of the public API.
 __all__ = ["climatology_rolling", "climatology_2015", "climatology_plus_trend_2015",
