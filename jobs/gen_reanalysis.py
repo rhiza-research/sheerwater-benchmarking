97f4b8412b547aae011aa2942b8b483292619e87
Genevieve Flaspohler <geflaspohler@gmail.com>
2024-11-01 16:09:53 -0700

    Fixed chunking in crps

--- jobs/gen_reanalysis.py
+++ jobs/gen_reanalysis.py
@@ -5,22 +5,24 @@ from sheerwater_benchmarking.reanalysis.era5 import era5_rolled, era5_daily, era
 
 
 vars = ["tmp2m", "precip"]
-grids = ['global0_25']
+# grids = ['global0_25']
+grids = ['global1_5']
 # grids = ['global0_25', 'global1_5']
 regions = ['global']
 aggs = [14, 7]
+# aggs = [14, 7]
 masks = ["lsm"]
-# anoms = [True, False]
-anoms = [False]
-clim_params = {'first_year': 1991, 'last_year': 2020}
+anoms = [True, False]
+# anoms = [False]
+clim_params = {'first_year': 1985, 'last_year': 2014}
 
 start_time = "1979-01-01"
 end_time = "2024-11-01"
 
 UPDATE_DAILY = False
 UPDATE_DAILY_REGRID = False
-UPDATE_ROLLED = True
-UPDATE_AGG = False
+UPDATE_ROLLED = False
+UPDATE_AGG = True 
 
 FLAG = False
 for var, grid in product(vars, grids):
@@ -46,5 +48,7 @@ for var, grid in product(vars, grids):
                 ds = era5_agg(start_time, end_time, variable=var,
                               agg=agg, anom=anom, clim_params=clim_params,
                               grid=grid, mask=mask, region=region,
-                              recompute=True, remote=True, force_overwrite=True,
-                              remote_config={'name': 'genevieve'})
+                              remote=True, remote_config='xlarge_cluster', remote_name='lead-bias',
+                              force_overwrite=True,
+                              #   recompute=True, force_overwrite=True,
+                              )
