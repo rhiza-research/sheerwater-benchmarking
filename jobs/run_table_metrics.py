d5f6c60b13be40dcdb4dd705e643b9c78250c377
Genevieve Flaspohler <geflaspohler@gmail.com>
2024-11-08 10:52:07 -0800

    Added biweekly to table

--- jobs/run_table_metrics.py
+++ jobs/run_table_metrics.py
@@ -31,8 +31,17 @@ def run_metrics_table(combo):
                               time_grouping=time_grouping, grid=grid, region=region,
                               force_overwrite=True, filepath_only=filepath_only,
                               recompute=recompute, storage_backend=backend)
-    except: # noqa: E722
+    except:  # noqa: E722
         print(f"Failed to run metric {grid} {variable} {metric} \
+                {region} {baseline} {time_grouping}: {traceback.format_exc()}")
+
+    try:
+        biweekly_summary_metrics_table(start_time, end_time, variable, "era5", metric, baseline=baseline,
+                                       time_grouping=time_grouping, grid=grid, region=region,
+                                       force_overwrite=True, filepath_only=filepath_only,
+                                       recompute=recompute, storage_backend=backend)
+    except:  # noqa: E722
+        print(f"Failed to run biweekly metric {grid} {variable} {metric} \
                 {region} {baseline} {time_grouping}: {traceback.format_exc()}")
 
 
