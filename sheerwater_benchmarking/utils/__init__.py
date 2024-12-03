4780bea08ba7e9c8b4123b28f638fa2728144082
Genevieve Flaspohler <geflaspohler@gmail.com>
2024-12-03 10:52:38 -0800

    Added gap secret

--- sheerwater_benchmarking/utils/__init__.py
+++ sheerwater_benchmarking/utils/__init__.py
@@ -1,7 +1,7 @@
 """Utility functions for benchmarking."""
 from .caching import cacheable
 from .remote import dask_remote, start_remote
-from .secrets import cdsapi_secret, ecmwf_secret, salient_secret, salient_auth, tahmo_secret
+from .secrets import cdsapi_secret, ecmwf_secret, salient_secret, salient_auth, tahmo_secret, gap_secret
 from .data_utils import (apply_mask, roll_and_agg, regrid, clip_region,
                          lon_base_change, get_globe_slice, get_anomalies, is_valid)
 from .general_utils import (load_netcdf, write_zarr, load_zarr, load_object)
@@ -24,6 +24,7 @@ __all__ = [
     "ecmwf_secret",
     "salient_secret",
     "tahmo_secret",
+    "gap_secret",
     "salient_auth",
     "apply_mask",
     "roll_and_agg",
