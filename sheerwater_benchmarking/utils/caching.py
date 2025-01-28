4eddf9c124bd48adfba41d37c0f724758f45b3ff
Adam Selker <adamselker@gmail.com>
2025-01-27 20:26:16 -0800

    Combine a few branches

--- sheerwater_benchmarking/utils/caching.py
+++ sheerwater_benchmarking/utils/caching.py
@@ -908,20 +908,16 @@ def cacheable(data_type, cache_args, timeseries=None, chunking=None, chunk_by_ar
             else:
                 # Do the time series filtering
                 if timeseries is not None:
-                    if data_type == "array":
-                        match_time = [t for t in tl if t in ds.dims]
-                        if len(match_time) == 0:
-                            raise RuntimeError(
-                                f"Timeseries array must return a dimension named {tl} for slicing."
-                            )
-                    elif data_type == "tabular":
-                        match_time = [t for t in tl if t in ds.columns]
-                        if len(match_time) == 0:
-                            raise RuntimeError(
-                                f"Timeseries must have a column named {tl} for slicing."
-                            )
-                    else:
-                        raise ValueError("Timeseries is only supported for array and tabular data")
+
+                    match_time = [
+                        t
+                        for t in tl
+                        if t in (ds.dims if data_type == "array" else ds.columns)
+                    ]
+                    if len(match_time) == 0:
+                        raise RuntimeError(
+                            f"Timeseries must have a dimension or column named {tl} for slicing."
+                        )
 
                     time_col = match_time[0]
 
