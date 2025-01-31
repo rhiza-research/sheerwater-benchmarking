5d50185d3963458619b0d654f54729645d158101
Joshua Adkins <josh@rhizaresearch.org>
2025-01-31 15:27:34 -0800

    untest fuxi forecasts

--- sheerwater_benchmarking/forecasts/fuxi.py
+++ sheerwater_benchmarking/forecasts/fuxi.py
@@ -10,6 +10,14 @@ import xarray as xr
 import os
 import shutil
 
+from sheerwater_benchmarking.utils import (dask_remote, cacheable,
+                                           apply_mask, clip_region,
+                                           lon_base_change,
+                                           regrid, get_variable,
+                                           target_date_to_forecast_date,
+                                           shift_forecast_date_to_target_date, lead_to_agg_days)
+
+
 @dask_remote
 @cacheable(data_type='array', cache_args=['date'])
 def fuxi_single_forecast(date):
@@ -28,7 +36,7 @@ def fuxi_single_forecast(date):
         return None
 
     # unzip the file
-    with py7zr.SevenZipFile(path, mode='r') as zip: 
+    with py7zr.SevenZipFile(path, mode='r') as zip:
         print(f"Extracting {path} to {target_path}")
         zip.extractall(path=target_path)
 
@@ -71,7 +79,7 @@ def fuxi_single_forecast(date):
     return ds
 
 @dask_remote
-@cacheable(data_type='array', cache_args=[], timeseries='time', 
+@cacheable(data_type='array', cache_args=[], timeseries='time',
            chunking={'lat': 121, 'lon': 240, 'lead_time': 14, 'time': 2, 'member': 51})
 def fuxi_raw(start_time, end_time):
     dates = pd.date_range(start_time, end_time)
@@ -89,7 +97,7 @@ def fuxi_raw(start_time, end_time):
     if len(data) == 0:
         print("No data found.")
         return None
- 
+
     ds = xr.open_mfdataset(data,
                           engine='zarr',
                           combine="by_coords",
@@ -99,3 +107,91 @@ def fuxi_raw(start_time, end_time):
     ds = ds.rename({'tp': 'precip', 't2m': 'tmp2m'})
 
     return ds
+
+@dask_remote
+@cacheable(data_type='array',
+           cache_args=['grid'],
+           cache_disable_if={'grid':'global1_5'},
+           chunking={'lat': 121, 'lon': 240, 'lead_time': 14, 'time': 2, 'member': 51})
+def fuxi_gridded(start_time, end_time, grid='global1_5'):
+
+    ds = fuxi_raw(start_time, end_time)
+
+    if grid != 'global1_5'
+        ds = regrid(ds, grid, base='base180', method='conservative',
+                    output_chunks={"lat": 721, "lon": 1440})
+
+    return ds
+
+@dask_remote
+@cacheable(data_type='array',
+           cache_args=['agg_days', 'prob_type', 'grid'],
+           cache_disable_if={'grid':'global1_5'},
+           chunking={'lat': 121, 'lon': 240, 'lead_time': 14, 'time': 2, 'member': 51})
+def fuxi_rolled(start_time, end_time, agg_days=7, prob_type='probabilistic', grid='global1_5'):
+    ds = fuxi_gridded(start_time, end_timel, grid)
+
+    # If deterministic average across the members
+    if prob_type = 'deterministic':
+        ds = ds.mean(dim='member')
+        ds = ds.assign_attrs(prob_type="deterministic")
+    else:
+        ds = ds.assign_attrs(prob_type="ensemble")
+
+    ds = roll_and_agg(ds, agg=agg_days, agg_col="lead_time", agg_fn="mean")
+
+    return ds
+
+@dask_remote
+@cacheable(data_type='array',
+           timeseries='time',
+           cache=False,
+           cache_args=['variable', 'lead', 'prob_type', 'grid', 'mask', 'region'])
+def fuxi(start_time, end_time, variable, lead, prob_type='deterministic',
+         grid='global1_5', mask='lsm', region="global"):
+
+    """Standard format forecast data for daily forecasts."""
+    lead_params = {
+        "week1": ('weekly', 0),
+        "week2": ('weekly', 7),
+        "week3": ('weekly', 14),
+        "week4": ('weekly', 21),
+        "week5": ('weekly', 28),
+        "week6": ('weekly', 35),
+        "weeks12": ('biweekly', 0),
+        "weeks23": ('biweekly', 7),
+        "weeks34": ('biweekly', 14),
+        "weeks45": ('biweekly', 21),
+        "weeks56": ('biweekly', 28),
+    }
+    time_group, lead_offset_days = lead_params.get(lead, (None, None))
+    if time_group is None:
+        raise NotImplementedError(f"Lead {lead} not implemented for ECMWF debiased forecasts.")
+
+    # Convert start and end time to forecast start and end based on lead time
+    forecast_start = target_date_to_forecast_date(start_time, lead)
+    forecast_end = target_date_to_forecast_date(end_time, lead)
+
+    # Get the data with the right days
+    agg_days = lead_to_agg_days(lead)
+    ds = fuxi_rolled(forecast_start, forecast_end, prob_type=prob_type, agg_days=agg_days, grid=grid)
+
+    # Get the right variable
+    ds = ds[variable].to_dataset()
+
+    # Get specific lead
+    lead_shift = np.timedelta64(lead_offset_days, 'D')
+    ds = ds.sel(lead_time=lead_shift)
+
+    # Time shift - we want target date, instead of forecast date
+    ds = shift_forecast_date_to_target_date(ds, 'time', lead)
+
+    # Apply masking
+    ds = apply_mask(ds, mask, var=variable, grid=grid)
+
+    # Clip to specified region
+    ds = clip_region(ds, region=region)
+
+    return ds
+
+
