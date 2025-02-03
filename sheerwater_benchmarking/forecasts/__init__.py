8fa709a2c1fd12bffc692856850b88cf74b99392
Joshua Adkins <josh@rhizaresearch.org>
2025-02-03 10:33:52 -0800

    add fuxi evaluation

--- sheerwater_benchmarking/forecasts/__init__.py
+++ sheerwater_benchmarking/forecasts/__init__.py
@@ -2,9 +2,10 @@
 from .salient import salient
 from .abc import perpp
 from .ecmwf_er import ecmwf_ifs_er, ecmwf_ifs_er_debiased
+from .fuxi import fuxi
 
 # Use __all__ to define what is part of the public API.
-__all__ = ["ecmwf_abc_wb", "salient", "ecmwf_ifs_er_debiased", "ecmwf_ifs_er", "perpp"]
+__all__ = ["ecmwf_abc_wb", "salient", "ecmwf_ifs_er_debiased", "ecmwf_ifs_er", "perpp", "fuxi"]
 
 # Define which are proper forecasts to be benchmarked
-__forecasts__ = ["salient", "ecmwf_ifs_er_debiased", "ecmwf_ifs_er", "perpp"]
+__forecasts__ = ["salient", "ecmwf_ifs_er_debiased", "ecmwf_ifs_er", "perpp", "fuxi"]
--- sheerwater_benchmarking/forecasts/fuxi.py
+++ sheerwater_benchmarking/forecasts/fuxi.py
@@ -9,13 +9,14 @@ import dask
 import xarray as xr
 import os
 import shutil
+import numpy as np
 
 from sheerwater_benchmarking.utils import (dask_remote, cacheable,
                                            apply_mask, clip_region,
                                            lon_base_change,
                                            regrid, get_variable,
                                            target_date_to_forecast_date,
-                                           shift_forecast_date_to_target_date, lead_to_agg_days)
+                                           shift_forecast_date_to_target_date, lead_to_agg_days, roll_and_agg)
 
 
 @dask_remote
@@ -52,6 +53,7 @@ def fuxi_single_forecast(date):
     # Transform and drop dataa
     print("Opening dataset")
     ds = xr.open_mfdataset(files, engine='netcdf4', preprocess=pre)
+    return ds
 
     ds = ds['__xarray_dataarray_variable__'].to_dataset(dim='channel')
 
@@ -67,6 +69,8 @@ def fuxi_single_forecast(date):
         'tcwv'
     ]
 
+    print(ds)
+
     ds = ds[variables]
     ds = ds.compute()
 
@@ -81,16 +85,22 @@ def fuxi_single_forecast(date):
 @dask_remote
 @cacheable(data_type='array', cache_args=[], timeseries='time',
            chunking={'lat': 121, 'lon': 240, 'lead_time': 14, 'time': 2, 'member': 51})
-def fuxi_raw(start_time, end_time):
+def fuxi_raw(start_time, end_time, delayed=False):
     dates = pd.date_range(start_time, end_time)
 
     datasets = []
     for date in dates:
         date = date.date()
-        ds = fuxi_single_forecast(date, filepath_only=True)
+        if delayed:
+            ds = dask.delayed(fuxi_single_forecast)(date, filepath_only=True)
+        else:
+            ds = fuxi_single_forecast(date, filepath_only=True)
+
         datasets.append(ds)
 
-    #datasets = dask.compute(*datasets)
+    if delayed:
+        datasets = dask.compute(*datasets)
+
     print(datasets)
 
     data = [d for d in datasets if d is not None]
@@ -108,31 +118,31 @@ def fuxi_raw(start_time, end_time):
 
     return ds
 
+#@dask_remote
+#@cacheable(data_type='array',
+#           timeseries='time',
+#           cache_args=['grid'],
+#           cache_disable_if={'grid':'global1_5'},
+#           chunking={'lat': 121, 'lon': 240, 'lead_time': 14, 'time': 2, 'member': 51})
+#def fuxi_gridded(start_time, end_time, grid='global1_5'):
+#
+#    ds = fuxi_raw(start_time, end_time)
+#
+#    if grid != 'global1_5':
+#        ds = regrid(ds, grid, base='base180', method='conservative',
+#                    output_chunks={"lat": 721, "lon": 1440})
+#
+#    return ds
+
 @dask_remote
 @cacheable(data_type='array',
-           cache_args=['grid'],
-           cache_disable_if={'grid':'global1_5'},
+           cache_args=['agg_days', 'prob_type'],
            chunking={'lat': 121, 'lon': 240, 'lead_time': 14, 'time': 2, 'member': 51})
-def fuxi_gridded(start_time, end_time, grid='global1_5'):
-
+def fuxi_rolled(start_time, end_time, agg_days=7, prob_type='probabilistic'):
     ds = fuxi_raw(start_time, end_time)
 
-    if grid != 'global1_5'
-        ds = regrid(ds, grid, base='base180', method='conservative',
-                    output_chunks={"lat": 721, "lon": 1440})
-
-    return ds
-
-@dask_remote
-@cacheable(data_type='array',
-           cache_args=['agg_days', 'prob_type', 'grid'],
-           cache_disable_if={'grid':'global1_5'},
-           chunking={'lat': 121, 'lon': 240, 'lead_time': 14, 'time': 2, 'member': 51})
-def fuxi_rolled(start_time, end_time, agg_days=7, prob_type='probabilistic', grid='global1_5'):
-    ds = fuxi_gridded(start_time, end_timel, grid)
-
     # If deterministic average across the members
-    if prob_type = 'deterministic':
+    if prob_type == 'deterministic':
         ds = ds.mean(dim='member')
         ds = ds.assign_attrs(prob_type="deterministic")
     else:
@@ -149,6 +159,9 @@ def fuxi_rolled(start_time, end_time, agg_days=7, prob_type='probabilistic', gri
            cache_args=['variable', 'lead', 'prob_type', 'grid', 'mask', 'region'])
 def fuxi(start_time, end_time, variable, lead, prob_type='deterministic',
          grid='global1_5', mask='lsm', region="global"):
+
+    if grid != 'global1_5':
+        raise NotImplementedError("Only 1.5 grid implemented for FuXi.")
 
     """Standard format forecast data for daily forecasts."""
     lead_params = {
@@ -174,13 +187,28 @@ def fuxi(start_time, end_time, variable, lead, prob_type='deterministic',
 
     # Get the data with the right days
     agg_days = lead_to_agg_days(lead)
-    ds = fuxi_rolled(forecast_start, forecast_end, prob_type=prob_type, agg_days=agg_days, grid=grid)
+    ds = fuxi_rolled(forecast_start, forecast_end, prob_type=prob_type, agg_days=agg_days)
+    ds = lon_base_change(ds)
+
 
     # Get the right variable
+    attrs = ds.attrs
     ds = ds[variable].to_dataset()
+    ds = ds.assign_attrs(attrs)
+
+    # flip the lat dim
+    ds = ds.reindex(lat=list(reversed(ds.lat)))
+
+    # convert to mm
+    #if variable == 'precip':
+    #    ds['precip'] = ds['precip'] * 1000
+
+    # Convert from kelvin
+    if variable == 'tmp2m':
+        ds['tmp2m'] = ds['tmp2m'] - 273.15
 
     # Get specific lead
-    lead_shift = np.timedelta64(lead_offset_days, 'D')
+    lead_shift = np.timedelta64(lead_offset_days + 1, 'D')
     ds = ds.sel(lead_time=lead_shift)
 
     # Time shift - we want target date, instead of forecast date
