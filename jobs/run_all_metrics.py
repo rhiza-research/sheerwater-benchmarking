f9b5f9e837b7c43713c5f3843cf3e7bcd54c74cc
Joshua Adkins <josh@rhizaresearch.org>
2025-02-08 13:25:22 -0800

    fix import

--- jobs/run_all_metrics.py
+++ jobs/run_all_metrics.py
@@ -3,7 +3,7 @@
 import itertools
 import traceback
 
-from sheerwater_benchmarking.metrics import global_metric
+from sheerwater_benchmarking.metrics import global_metric, grouped_metric, summary_metrics_table
 from sheerwater_benchmarking.utils import start_remote
 from jobs import parse_args, run_in_parallel, prune_metrics
 
