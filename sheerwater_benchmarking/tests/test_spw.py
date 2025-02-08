7444efdb1e12b94095f9305b4eb0c3f816d63306
Genevieve Flaspohler <geflaspohler@gmail.com>
2025-02-07 21:44:55 -0800

    Updated testing

--- sheerwater_benchmarking/tests/test_spw.py
+++ sheerwater_benchmarking/tests/test_spw.py
@@ -2,7 +2,7 @@
 import numpy as np
 from sheerwater_benchmarking.metrics import get_datasource_fn
 from sheerwater_benchmarking.utils import start_remote
-from sheerwater_benchmarking.forecasts.ecmwf_er import ifs_extended_range_spw
+from sheerwater_benchmarking.forecasts.ecmwf_er import ecmwf_ifs_spw
 from sheerwater_benchmarking.baselines.climatology import climatology_spw
 
 
@@ -16,7 +16,7 @@ def test_spw():
         for prob_type in prob_types:
             fn = get_datasource_fn(forecast)
             datasets.append(fn(start_time, end_time,
-                            'rainy_onset',
+                               'rainy_onset',
                                lead='day11',
                                prob_type=prob_type,
                                grid='global1_5', mask='lsm', region='kenya'))
@@ -25,24 +25,29 @@ def test_spw():
         assert ds.time.size == 14
         assert np.issubdtype(ds.rainy_onset.dtype, np.datetime64)
 
-    # Test calling SPW and getting a raw probability time series for ECMWF IFS ER (debiased and non-debiased)
-    datasets = []
-    for debiased in [False, True]:
-        datasets.append(ifs_extended_range_spw(start_time, end_time, 'day11',  debiased=debiased,
-                                               prob_type='probabilistic', prob_threshold=None,
-                                               onset_group=None, aggregate_group=None,
-                                               grid='global1_5', mask='lsm', region='kenya'))
-
-    for ds in datasets:
-        assert ds.time.size == 730
+    for data in ["era5", "chirps", "ghcn", "imerg", "tahmo"]:
+        fn = get_datasource_fn(data)
+        ds = fn(start_time, end_time,
+                'rainy_onset', agg_days=None,
+                grid='global1_5', mask='lsm', region='kenya')
+        if data != 'tahmo':
+            assert ds.time.size == 14
         assert np.issubdtype(ds.rainy_onset.dtype, np.datetime64)
+
+    # Test calling SPW and getting a raw probability time series for ECMWF IFS ER
+    ds = ecmwf_ifs_spw(start_time, end_time, 'day11',
+                       prob_type='probabilistic', prob_threshold=None,
+                       onset_group=None, aggregate_group=None,
+                       grid='global1_5', mask='lsm', region='kenya')
+    assert ds.time.size == 730
+    assert np.issubdtype(ds.rainy_onset.dtype, np.datetime64)
 
     # Test calling SPW and getting a raw probability time series for climatology
     ds = climatology_spw(first_year=2004, last_year=2015,
                          prob_type='probabilistic', prob_threshold=None,
                          onset_group=None, aggregate_group=None,
                          grid='global1_5', mask='lsm', region='kenya')
-    assert ds.time.size == 730
+    assert ds.time.size == 2557
     assert np.issubdtype(ds.rainy_onset.dtype, np.datetime64)
 
 
