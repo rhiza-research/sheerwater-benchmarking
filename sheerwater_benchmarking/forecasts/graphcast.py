fecc8629dd8946a77b8cdba31835e1773893197f
Genevieve Flaspohler <geflaspohler@gmail.com>
2025-02-12 13:51:01 -0800

    Fixes to graphcast

--- sheerwater_benchmarking/forecasts/graphcast.py
+++ sheerwater_benchmarking/forecasts/graphcast.py
@@ -6,11 +6,13 @@ import numpy as np
 import pandas as pd
 import dateparser
 
-from sheerwater_benchmarking.utils import (dask_remote, cacheable,
+from sheerwater_benchmarking.utils import (dask_remote, cacheable, plot_ds,
                                            apply_mask, clip_region,
                                            lon_base_change,
                                            target_date_to_forecast_date,
-                                           shift_forecast_date_to_target_date, lead_to_agg_days, roll_and_agg, regrid)
+                                           shift_forecast_date_to_target_date,
+                                           lead_to_agg_days, roll_and_agg, regrid)
+
 
 @dask_remote
 @cacheable(data_type='array',
@@ -22,29 +24,47 @@ from sheerwater_benchmarking.utils import (dask_remote, cacheable,
 def graphcast_daily(start_time, end_time, variable, grid='global0_25'):
     "graphcast Daily."
     # Read the three years for gcloud
-    ds1 = xr.open_zarr('gs://weathernext/59572747_4_0/zarr/99140631_1_2020_to_2021/forecasts_10d/date_range_2019-12-01_2021-01-22_6_hours.zarr/')
-    ds1 = ds1.rename({'prediction_timedelta': 'lead_time', '2m_temperature': 'tmp2m', 'total_precipitation_6hr' : 'precip'})
+    ds1 = xr.open_zarr(
+        'gs://weathernext/59572747_4_0/zarr/99140631_1_2020_to_2021/forecasts_10d/date_range_2019-12-01_2021-01-22_6_hours.zarr/',
+        decode_timedelta=True)
+    ds1 = ds1.rename({'prediction_timedelta': 'lead_time',
+                     '2m_temperature': 'tmp2m', 'total_precipitation_6hr': 'precip'})
     ds1 = ds1[[variable]]
     ds1 = ds1.sel({'time': slice(pd.to_datetime("2019-12-01"), pd.to_datetime("2020-11-30"))})
 
-    ds2 = xr.open_zarr('gs://weathernext/59572747_4_0/zarr/99140631_2_2021_to_2022/forecasts_10d/date_range_2020-12-01_2022-01-22_6_hours.zarr')
-    ds2 = ds2.rename({'prediction_timedelta': 'lead_time', '2m_temperature': 'tmp2m', 'total_precipitation_6hr' : 'precip'})
+    ds2 = xr.open_zarr(
+        'gs://weathernext/59572747_4_0/zarr/99140631_2_2021_to_2022/forecasts_10d/date_range_2020-12-01_2022-01-22_6_hours.zarr',
+        decode_timedelta=True)
+    ds2 = ds2.rename({'prediction_timedelta': 'lead_time',
+                     '2m_temperature': 'tmp2m', 'total_precipitation_6hr': 'precip'})
     ds2 = ds2[[variable]]
     ds2 = ds2.sel({'time': slice(pd.to_datetime("2020-12-01"), pd.to_datetime("2021-12-31"))})
 
-    ds3 = xr.open_zarr('gs://weathernext/59572747_4_0/zarr/132880704_2022_to_2023/1/forecasts_10d/date_range_2022-01-01_2023-01-01_12_hours.zarr')
-    ds3 = ds3.rename({'prediction_timedelta': 'lead_time', '2m_temperature': 'tmp2m', 'total_precipitation_6hr' : 'precip'})
+    ds3 = xr.open_zarr(
+        'gs://weathernext/59572747_4_0/zarr/132880704_2022_to_2023/1/forecasts_10d/date_range_2022-01-01_2023-01-01_12_hours.zarr',
+        decode_timedelta=True)
+    ds3 = ds3.rename({'prediction_timedelta': 'lead_time',
+                     '2m_temperature': 'tmp2m', 'total_precipitation_6hr': 'precip'})
     ds3 = ds3[[variable]]
     ds3 = ds3.sel({'time': slice(pd.to_datetime("2022-01-01"), pd.to_datetime("2023-01-01"))})
 
-    ## concat them together
+    # concat them together
     ds = xr.concat([ds1, ds2, ds3], 'time')
 
-    # Aggregate into daily bins
-    if variable == 'precip':
-        ds = ds.resample(lead_time='1D').sum()
+    # Convert units
+    K_const = 273.15
+    if variable == 'tmp2m':
+        ds[variable] = ds[variable] - K_const
+        ds.attrs.update(units='C')
+        ds = ds.resample(time='1D').mean(dim='time')
+    elif variable == 'precip':
+        ds[variable] = ds[variable] * 1000.0
+        ds.attrs.update(units='mm')
+        ds = ds.resample(time='1D').sum(dim='time')
+        # Can't have precip less than zero (there are some very small negative values)
+        ds = np.maximum(ds, 0)
     else:
-        ds = ds.resample(lead_time='1D').mean()
+        raise ValueError(f"Variable {variable} not implemented.")
 
     # Shift the lead back 6 hours to be aligned
     ds['lead_time'] = ds['lead_time'] - np.timedelta64(6, 'h')
@@ -61,7 +81,6 @@ def graphcast_daily(start_time, end_time, variable, grid='global0_25'):
     if grid != 'global0_25':
         ds = regrid(ds, grid, base='base180', method='conservative',
                     output_chunks={"lat": 721, "lon": 1440})
-
     return ds
 
 
@@ -70,14 +89,41 @@ def graphcast_daily(start_time, end_time, variable, grid='global0_25'):
            cache_args=['variable', 'agg_days', 'grid'],
            timeseries='time',
            cache=True,
+           chunking={"lat": 121, "lon": 240, "lead_time": 10, "time": 100},
            validate_cache_timeseries=False)
 def graphcast_rolled(start_time, end_time, variable, agg_days, grid='global0_25'):
     """ graphcast Rolled. """
     ds = graphcast_daily(start_time, end_time, variable, grid)
-
+    if 'units' not in ds.attrs:  # units haven't been converted yet
+        # Convert units
+        K_const = 273.15
+        if variable == 'tmp2m':
+            ds[variable] = ds[variable] - K_const
+            ds.attrs.update(units='C')
+        elif variable == 'precip':
+            ds[variable] = ds[variable] * 1000.0
+            ds.attrs.update(units='mm')
+            # Can't have precip less than zero (there are some very small negative values)
+            ds = np.maximum(ds, 0)
+        else:
+            raise ValueError(f"Variable {variable} not implemented.")
     ds = roll_and_agg(ds, agg=agg_days, agg_col="lead_time", agg_fn="mean")
-
     return ds
+
+
+def _process_lead(variable, lead):
+    """Helper function for interpreting lead for Graphcast forecasts."""
+    if variable not in ['precip', 'tmp2m']:
+        raise NotImplementedError(f"Variable {variable} not implemented for Graphcast forecasts.")
+    lead_params = {}
+    for i in range(10):
+        lead_params[f"day{i+1}"] = i
+    lead_params["week1"] = 0
+    lead_offset_days = lead_params.get(lead, None)
+    if lead_offset_days is None:
+        raise NotImplementedError(f"Lead {lead} not implemented for Graphcast {variable} forecasts.")
+    agg_days = lead_to_agg_days(lead)
+    return agg_days, lead_offset_days
 
 
 @dask_remote
@@ -86,27 +132,23 @@ def graphcast_rolled(start_time, end_time, variable, agg_days, grid='global0_25'
            cache=False,
            cache_args=['variable', 'lead', 'prob_type', 'grid', 'mask', 'region'])
 def graphcast(start_time, end_time, variable, lead, prob_type='deterministic',
-         grid='global1_5', mask='lsm', region="global"):
+              grid='global1_5', mask='lsm', region="global"):
     """Final graphcast interface"""
     if prob_type != 'deterministic':
         raise NotImplementedError("Only deterministic forecast implemented for graphcast")
 
-    """Standard format forecast data for daily forecasts."""
-    lead_params = {
-        "week1": ('weekly', 0),
-    }
-
-    time_group, lead_offset_days = lead_params.get(lead, (None, None))
-    if time_group is None:
-        raise NotImplementedError(f"Lead {lead} not implemented for graphcast forecasts.")
+    # Process the lead
+    agg_days, lead_offset_days = _process_lead(variable, lead)
 
     # Convert start and end time to forecast start and end based on lead time
     forecast_start = target_date_to_forecast_date(start_time, lead)
     forecast_end = target_date_to_forecast_date(end_time, lead)
 
     # Get the data with the right days
-    agg_days = lead_to_agg_days(lead)
-    ds = graphcast_rolled(start_time, end_time, variable, agg_days=agg_days, grid=grid)
+    ds = graphcast_rolled(forecast_start, forecast_end, variable, agg_days=agg_days, grid=grid,
+                          recompute=True)
+    import pdb
+    pdb.set_trace()
     ds = ds.assign_attrs(prob_type="deterministic")
 
     # Get specific lead
