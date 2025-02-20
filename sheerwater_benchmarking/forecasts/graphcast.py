2f1a73029bde9cd3f9773d9ed9f66e0ad71f4114
Joshua Adkins <josh@rhizaresearch.org>
2025-02-20 11:28:58 -0800

    take graphcast from other branch

--- sheerwater_benchmarking/forecasts/graphcast.py
+++ sheerwater_benchmarking/forecasts/graphcast.py
@@ -1,74 +1,12 @@
 """Interface for graphcast forecasts."""
-<<<<<<< HEAD
-import os
-import dask
 import xarray as xr
 import numpy as np
 import pandas as pd
-import dateparser
-=======
-import xarray as xr
-import numpy as np
-import pandas as pd
->>>>>>> adkins/neuralgcm
 
 from sheerwater_benchmarking.utils import (dask_remote, cacheable,
                                            apply_mask, clip_region,
                                            lon_base_change,
                                            target_date_to_forecast_date,
-<<<<<<< HEAD
-                                           shift_forecast_date_to_target_date, lead_to_agg_days, roll_and_agg, regrid)
-
-@dask_remote
-@cacheable(data_type='array',
-           cache_args=['variable', 'grid'],
-           timeseries='time',
-           cache=True,
-           cache_disable_if={'grid': 'global0_25'},
-           validate_cache_timeseries=False)
-def graphcast_daily(start_time, end_time, variable, grid='global0_25'):
-    "graphcast Daily."
-    # Read the three years for gcloud
-    ds1 = xr.open_zarr('gs://weathernext/59572747_4_0/zarr/99140631_1_2020_to_2021/forecasts_10d/date_range_2019-12-01_2021-01-22_6_hours.zarr/')
-    ds1 = ds1.rename({'prediction_timedelta': 'lead_time', '2m_temperature': 'tmp2m', 'total_precipitation_6hr' : 'precip'})
-    ds1 = ds1[[variable]]
-    ds1 = ds1.sel({'time': slice(pd.to_datetime("2019-12-01"), pd.to_datetime("2020-11-30"))})
-
-    ds2 = xr.open_zarr('gs://weathernext/59572747_4_0/zarr/99140631_2_2021_to_2022/forecasts_10d/date_range_2020-12-01_2022-01-22_6_hours.zarr')
-    ds2 = ds2.rename({'prediction_timedelta': 'lead_time', '2m_temperature': 'tmp2m', 'total_precipitation_6hr' : 'precip'})
-    ds2 = ds2[[variable]]
-    ds2 = ds2.sel({'time': slice(pd.to_datetime("2020-12-01"), pd.to_datetime("2021-12-31"))})
-
-    ds3 = xr.open_zarr('gs://weathernext/59572747_4_0/zarr/132880704_2022_to_2023/1/forecasts_10d/date_range_2022-01-01_2023-01-01_12_hours.zarr')
-    ds3 = ds3.rename({'prediction_timedelta': 'lead_time', '2m_temperature': 'tmp2m', 'total_precipitation_6hr' : 'precip'})
-    ds3 = ds3[[variable]]
-    ds3 = ds3.sel({'time': slice(pd.to_datetime("2022-01-01"), pd.to_datetime("2023-01-01"))})
-
-    ## concat them together
-    ds = xr.concat([ds1, ds2, ds3], 'time')
-
-    # Aggregate into daily bins
-    if variable == 'precip':
-        ds = ds.resample(lead_time='1D').sum()
-    else:
-        ds = ds.resample(lead_time='1D').mean()
-
-    # Shift the lead back 6 hours to be aligned
-    ds['lead_time'] = ds['lead_time'] - np.timedelta64(6, 'h')
-    dates = pd.date_range("2019-12-01", "2022-12-31")
-    dates = [pd.to_datetime(date) for date in dates]
-
-    ds = ds.sel(time=dates)
-
-    # Convert lat/lon
-    ds = lon_base_change(ds)
-    ds = ds.sortby(ds.lat)
-
-    # Regrid
-    if grid != 'global0_25':
-        ds = regrid(ds, grid, base='base180', method='conservative',
-                    output_chunks={"lat": 721, "lon": 1440})
-=======
                                            shift_forecast_date_to_target_date,
                                            lead_to_agg_days, roll_and_agg, regrid)
 
