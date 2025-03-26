49ace072e45dab40f267e59a947ffe433722950e
Joshua Adkins <josh@rhizaresearch.org>
2025-03-26 13:51:44 -0700

    add recompute with depth

---
+++ sheerwater_benchmarking/tests/test_deep_cache.py
@@ -0,0 +1,43 @@
+"""Test the caching functions in the cacheable decorator."""
+import pytest
+import numpy as np
+import pandas as pd
+import xarray as xr
+from sheerwater_benchmarking.utils import cacheable, get_dates
+
+def test_deep_cache():
+    @cacheable(data_type='basic',
+               cache_args=[])
+    def deep_cached_func():  # noqa: ARG001
+        return np.random.randint(1000)
+
+    @cacheable(data_type='basic',
+               cache_args=[])
+    def deep_cached_func2():  # noqa: ARG001
+        return deep_cached_func() + np.random.randint(1000)
+
+    @cacheable(data_type='basic',
+               cache_args=[])
+    def deep_cached_func3():  # noqa: ARG001
+        return deep_cached_func2()
+
+    print("should match")
+    first = deep_cached_func3()
+    second = deep_cached_func3()
+    assert first == second
+
+    # now verify the recomputing all the way back works
+    fourth = deep_cached_func3(recompute='deep_cached_func', force_overwrite=True)
+    assert first != fourth
+
+    # now verify that just recompute the second one works
+    init = deep_cached_func()
+    second = fourth - init
+    final = deep_cached_func3(recompute='deep_cached_func2', force_overwrite=True)
+    dsecond = final - init
+    assert second != dsecond
+    init2 = deep_cached_func()
+    assert init == init2
+
+if __name__ == "__main__":
+    test_deep_cache()
--- sheerwater_benchmarking/utils/caching.py
+++ sheerwater_benchmarking/utils/caching.py
@@ -4,6 +4,7 @@ import datetime
 import dateparser
 from functools import wraps
 from inspect import signature, Parameter
+import inspect
 import logging
 import hashlib
 
@@ -473,6 +474,7 @@ def cache_exists(backend, cache_path, verify_path=None, local=False, verify_cach
     else:
         raise ValueError(f'Unknown backend {backend}')
 
+global_recompute_var = None
 
 def cacheable(data_type, cache_args, timeseries=None, chunking=None, chunk_by_arg=None,
               auto_rechunk=False, cache=True, validate_cache_timeseries=True, cache_disable_if=None,
@@ -538,6 +540,8 @@ def cacheable(data_type, cache_args, timeseries=None, chunking=None, chunk_by_ar
         @wraps(func)
         def wrapper(*args, **kwargs):
             # Proper variable scope for the decorator args
+            global global_recompute_var
+
             nonlocal data_type, cache_args, timeseries, chunking, chunk_by_arg, \
                 auto_rechunk, cache, validate_cache_timeseries, cache_disable_if, \
                 backend, storage_backend, verify_cache
@@ -701,7 +705,51 @@ def cacheable(data_type, cache_args, timeseries=None, chunking=None, chunk_by_ar
                     print(f"Removing and retrying null cache {null_path}.")
                     fs.rm(null_path, recursive=True)
 
-            if not recompute and cache:
+
+            # If recompute is True -> recompute once -> don't set global flag, act like normal
+            # If recompute is a string or list -> recompute until function matches then set to false -> set recompute flag until match
+
+            recompute_now = False
+
+            if global_recompute_var is None:
+                if recompute is False:
+                    pass
+                elif recompute is True:
+                    print("Top level recompute requested. Recomputing once.")
+                    recompute_now = True
+                elif isinstance(recompute, str) or isinstance(recompute, list):
+                    if isinstance(recompute, str):
+                        recompute = [recompute]
+
+                    print(f'Recomputation of functions {recompute} requested. Recomputing until found')
+                    recompute_now = True
+
+                    # Check to see if we are one of the functions
+                    if func.__name__ in recompute:
+                        recompute.remove(func.__name__)
+
+                    # Set the global_recompute_var
+                    global_recompute_var = recompute
+
+                    if len(global_recompute_var) == 0:
+                        print("Done recomputing after this function.")
+                        global_recompute_var = None
+
+            elif global_recompute_var:
+                print(f"Global recompute var set to {global_recompute_var}")
+
+                recompute_now = True
+
+                # Check to see if we are one of the functions
+                if func.__name__ in global_recompute_var:
+                    global_recompute_var.remove(func.__name__)
+
+                if len(global_recompute_var) == 0:
+                    print("Done recomputing after this function.")
+                    global_recompute_var = None
+
+
+            if not recompute_now and cache:
                 if cache_exists(backend, cache_path, verify_path, local=local, verify_cache=verify_cache):
                     # Read the cache
                     print(f"Found cache for {cache_path}")
@@ -874,6 +922,8 @@ def cacheable(data_type, cache_args, timeseries=None, chunking=None, chunk_by_ar
 
                     if timeseries is not None and (start_time is None or end_time is None):
                         raise ValueError('Need to pass start and end time arguments when recomputing function.')
+
+
 
                     ##### IF NOT EXISTS ######
                     ds = func(*args, **kwargs)
