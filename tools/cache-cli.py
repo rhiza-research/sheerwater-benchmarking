4ba116370e8f52086654db66d801107d8bcbef52
Joshua Adkins <josh@rhizaresearch.org>
2025-05-09 15:30:09 -0700

    add cache rename utility

--- tools/cache-cli.py
+++ tools/cache-cli.py
@@ -1,6 +1,6 @@
 """CLI for managing caches."""
 import click
-from cache_utils import cache_delete, cache_list, cache_verify
+from cache_utils import cache_delete, cache_list, cache_verify, cache_rename
 
 
 @click.group()
@@ -62,23 +62,28 @@ def verify(backend, name, glob):
     if num is not None:
         click.echo(f"Successfully verified {num} files")
 
-
-@cache.group()
-def rename():
-    """Rename a cache, key, or value."""
-    pass
-
-
-@rename.command('value')
-def rename_value():
-    """Rename a value."""
-    pass
-
-
-@rename.command('cache')
-def rename_cache():
-    """Rename a cache."""
-    pass
+@cache.command()
+@click.option('--backend', '-b',
+              type=click.Choice(['zarr', 'delta', 'postgres', 'terracotta', 'pickle', 'parquet']),
+              required=True,
+              help="The backend to find the cache")
+@click.option('--old-name', '-n', type=str,
+              help="Name of the old cache - this is the name of the cacheable function",
+              required=True)
+@click.option('--new-name', '-w', type=str,
+              help="Name of the new cache - this is the name of the cacheable function",
+              required=True)
+@click.option('--glob', '-g', type=str,
+              help="Globable regex of the cache key to list - pass '*' to list all caches",
+              required=True)
+@click.option('--parallel', '-m', is_flag=True,
+              help="Tell to run in parallel",
+              default=False)
+def rename(backend, old_name, new_name, glob, parallel):
+    """Verify all the caches that match the given name and glob pattern."""
+    num = cache_rename(backend, old_name, new_name, glob, parallel)
+    if num is not None:
+        click.echo(f"Successfully verified {num} files")
 
 
 if __name__ == '__main__':
