59d6081780099b07a1c8c889649cc3d308e89159
aselker <aselker319@gmail.com>
2025-05-15 10:14:16 -0700

    Fix cacheable wrapper is-top-level detection (#76)
    * Test recompute=[a list]
    
    * Rename wrappers; check equality rather than substring
    
    * Use a global variable for top-lvl detection
    
    * Reformat to satisfy linter
    
    * Short-term debugging hacks
    
    * Revert "Short-term debugging hacks"
    
    This reverts commit b140337cb936e082a3fac2741f450f9a9a32f26f.
    
    * Revert "Reformat to satisfy linter"
    
    This reverts commit 6b9a907b19b19338b1ca302c7e34b289dff3c82c.
    
    * Revert "Use a global variable for top-lvl detection"
    
    This reverts commit c9482edea66976f967b76a41834e22ab68e4c7bc.
    
    * Test cache with @dask_remote
    
    ---------
    
    Co-authored-by: Adam Selker <adamselker@gmail.com>

--- sheerwater_benchmarking/tests/test_deep_cache.py
+++ sheerwater_benchmarking/tests/test_deep_cache.py
@@ -1,47 +1,63 @@
 """Test the caching functions in the cacheable decorator."""
+
 import numpy as np
-from sheerwater_benchmarking.utils import cacheable
+from sheerwater_benchmarking.utils import cacheable, dask_remote
 
 
 def test_deep_cache():
     """Test the deep cache recompute functionality."""
-    @cacheable(data_type='basic',
-               cache_args=[])
+
+    @cacheable(data_type="basic", cache_args=[])
     def deep_cached_func():  # noqa: ARG001
-        return np.random.randint(1000)
+        return np.random.randint(1000000)
 
-    @cacheable(data_type='basic',
-               cache_args=[])
+    @cacheable(data_type="basic", cache_args=[])
     def deep_cached_func2():  # noqa: ARG001
-        return deep_cached_func() + np.random.randint(1000)
+        return deep_cached_func() + np.random.randint(1000000)
 
-    @cacheable(data_type='basic',
-               cache_args=[])
+    @cacheable(data_type="basic", cache_args=[])
     def deep_cached_func3():  # noqa: ARG001
         return deep_cached_func2()
 
-    print("should match")
     first = deep_cached_func3()
     second = deep_cached_func3()
     assert first == second
 
     # now verify the recomputing all the way back works
-    fourth = deep_cached_func3(recompute=['deep_cached_func', 'deep_cached_func2'], force_overwrite=True)
+    fourth = deep_cached_func3(recompute=["deep_cached_func", "deep_cached_func2"], force_overwrite=True)
     assert first != fourth
 
     # now verify that just recompute the second one works
     init = deep_cached_func()
     second = fourth - init
-    final = deep_cached_func3(recompute='deep_cached_func2', force_overwrite=True)
+    final = deep_cached_func3(recompute="deep_cached_func2", force_overwrite=True)
     dsecond = final - init
     assert second != dsecond
     init2 = deep_cached_func()
     assert init == init2
 
+    # verify that recompute="_all" recomputes nested functions.
     first = deep_cached_func3()
-    second = deep_cached_func3(force_overwrite=True, recompute='_all')
+    second = deep_cached_func3(recompute="_all", force_overwrite=True)
     assert first != second
+
+    # verify that recompute=[f,g] recomputes both f and g.
+    first = deep_cached_func3()
+    second = deep_cached_func3(recompute=["deep_cached_func3", "deep_cached_func2"], force_overwrite=True)
+    assert first != second
+
+    # verify that recompute=[f,g] doesn't recompute anything but f and g.
+    first = deep_cached_func3()
+    second = deep_cached_func3(recompute=["deep_cached_func3", "deep_cached_func1"], force_overwrite=True)
+    assert first == second
+
+
+@dask_remote
+def test_remote_deep_cache():
+    """Identical to test_deep_cache(), but decorated with @dask_remote."""
+    test_deep_cache()
 
 
 if __name__ == "__main__":
     test_deep_cache()
+    test_remote_deep_cache(remote=False)
