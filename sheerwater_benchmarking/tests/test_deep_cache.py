87b34c9bd39bf232be01ab5776719df4b3f63c56
Adam Selker <adamselker@gmail.com>
2025-05-15 10:12:21 -0700

    Test cache with @dask_remote

--- sheerwater_benchmarking/tests/test_deep_cache.py
+++ sheerwater_benchmarking/tests/test_deep_cache.py
@@ -1,22 +1,21 @@
 """Test the caching functions in the cacheable decorator."""
+
 import numpy as np
-from sheerwater_benchmarking.utils import cacheable
+from sheerwater_benchmarking.utils import cacheable, dask_remote
 
 
 def test_deep_cache():
     """Test the deep cache recompute functionality."""
-    @cacheable(data_type='basic',
-               cache_args=[])
+
+    @cacheable(data_type="basic", cache_args=[])
     def deep_cached_func():  # noqa: ARG001
         return np.random.randint(1000000)
 
-    @cacheable(data_type='basic',
-               cache_args=[])
+    @cacheable(data_type="basic", cache_args=[])
     def deep_cached_func2():  # noqa: ARG001
         return deep_cached_func() + np.random.randint(1000000)
 
-    @cacheable(data_type='basic',
-               cache_args=[])
+    @cacheable(data_type="basic", cache_args=[])
     def deep_cached_func3():  # noqa: ARG001
         return deep_cached_func2()
 
@@ -25,13 +24,13 @@ def test_deep_cache():
     assert first == second
 
     # now verify the recomputing all the way back works
-    fourth = deep_cached_func3(recompute=['deep_cached_func', 'deep_cached_func2'], force_overwrite=True)
+    fourth = deep_cached_func3(recompute=["deep_cached_func", "deep_cached_func2"], force_overwrite=True)
     assert first != fourth
 
     # now verify that just recompute the second one works
     init = deep_cached_func()
     second = fourth - init
-    final = deep_cached_func3(recompute='deep_cached_func2', force_overwrite=True)
+    final = deep_cached_func3(recompute="deep_cached_func2", force_overwrite=True)
     dsecond = final - init
     assert second != dsecond
     init2 = deep_cached_func()
@@ -39,7 +38,7 @@ def test_deep_cache():
 
     # verify that recompute="_all" recomputes nested functions.
     first = deep_cached_func3()
-    second = deep_cached_func3(recompute='_all', force_overwrite=True)
+    second = deep_cached_func3(recompute="_all", force_overwrite=True)
     assert first != second
 
     # verify that recompute=[f,g] recomputes both f and g.
@@ -53,5 +52,12 @@ def test_deep_cache():
     assert first == second
 
 
+@dask_remote
+def test_remote_deep_cache():
+    """Identical to test_deep_cache(), but decorated with @dask_remote."""
+    test_deep_cache()
+
+
 if __name__ == "__main__":
     test_deep_cache()
+    test_remote_deep_cache(remote=False)
