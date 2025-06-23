5ac07abfe09584057267b974c043c85d6ac51daf
Joshua Adkins <josh@rhizaresearch.org>
2025-06-23 14:28:44 -0700

    add rip

--- sheerwater_benchmarking/utils/__init__.py
+++ sheerwater_benchmarking/utils/__init__.py
@@ -4,7 +4,7 @@ from .remote import dask_remote, start_remote
 from .secrets import cdsapi_secret, ecmwf_secret, salient_secret, salient_auth, tahmo_secret, gap_secret
 from .data_utils import (apply_mask, roll_and_agg, regrid, clip_region,
                          lon_base_change, get_globe_slice, get_anomalies, is_valid)
-from .general_utils import (load_netcdf, write_zarr, load_zarr, load_object, plot_ds, plot_ds_map)
+from .general_utils import (load_netcdf, write_zarr, load_zarr, load_object, plot_ds, plot_ds_map, run_in_parallel)
 
 from .space_utils import (get_grid, get_grid_ds, is_wrapped, get_region,
                           base360_to_base180, base180_to_base360, check_bases)
@@ -70,4 +70,5 @@ __all__ = [
     "first_satisfied_date",
     "date_mean",
     "doy_mean",
+    "run_in_parallel",
 ]
