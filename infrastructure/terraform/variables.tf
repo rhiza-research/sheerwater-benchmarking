42b48af08a87235e423e923253b9106d112e9375
Tristan Escalada <355457+tescalada@users.noreply.github.com>
2025-07-08 21:26:31 -0400

    Update Terraform configuration for Grafana ApplicationSet
    - Added a TODO for setting up the DNS record for the Grafana domain.
    - Removed unused variables related to GitHub organization and ArgoCD domain from the Terraform variables file.

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,24 +1,5 @@
 # ArgoCD Configuration Variables
 
-
-variable "github_org" {
-  description = "GitHub organization or username"
-  type        = string
-  default     = "rhiza-research"
-}
-
-variable "grafana_repo_name" {
-  description = "Name of the repository containing Grafana charts"
-  type        = string
-  default     = "sheerwater-benchmarking"
-}
-
-variable "argocd_domain" {
-  description = "Domain for ArgoCD UI"
-  type        = string
-  default     = "argocd.sheerwater.rhizaresearch.org"
-}
-
 variable "grafana_domain" {
   description = "Base domain for ephemeral Grafana instances"
   type        = string
