ff4bcad21b682daa9364d40104018ebeb2db48c6
Tristan Escalada <355457+tescalada@users.noreply.github.com>
2025-07-24 11:28:07 -0400

    Update Grafana configuration and add workspace management script
    - Revised README to clarify the purpose of the terraform-config directory and added a new terraform-grafana-datasources module for shared datasource configuration.
    - Enhanced main.tf in terraform-config with detailed comments on workspace naming conventions and added local variables for Grafana URL management.
    - Introduced reset_workspace.sh script to manage workspace deletion and creation, ensuring safety checks are in place.
    - Updated provider configurations and datasource settings to improve clarity and functionality for both production and ephemeral Grafana instances.

---
+++ infrastructure/terraform-config/reset_workspace.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+workspace=$1
+
+# fail if the workspace is default
+if [ "$workspace" == "default" ]; then
+    echo "Cannot delete the default workspace"
+    exit 1
+fi
+
+if [ -z "$workspace" ]; then
+    echo "No workspace provided"
+    echo "Usage: $0 <workspace>"
+    echo "Available workspaces:"
+    terraform workspace list
+    exit 1
+fi
+
+echo "Are you sure you want to delete the workspace $workspace? (y/n)"
+read confirm
+if [ "$confirm" != "y" ]; then
+    echo "Aborting..."
+    exit 1
+fi
+
+# Delete all resources in the workspace
+terraform workspace select default
+terraform workspace delete -force $workspace
+terraform workspace new $workspace
