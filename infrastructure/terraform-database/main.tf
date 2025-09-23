3d0a6ffe70e67f12b8c94157ac3f712a118853ef
Joshua Adkins <josh@rhizaresearch.org>
2025-09-22 17:10:25 -0700

    fix name of role

--- infrastructure/terraform-database/main.tf
+++ infrastructure/terraform-database/main.tf
@@ -134,6 +134,6 @@ resource "postgresql_grant" "write_public_terracottads" {
 ### Access for the github action to deploy terraform
 resource "google_project_iam_member" "access-terraform-state" {
   project = google_project.project.project_id
-  role    = "roles/secretmanager.secretAccess"
+  role    = "roles/secretmanager.secretAccessor"
   member = "serviceAccount:${var.service_account_email}"
 }
