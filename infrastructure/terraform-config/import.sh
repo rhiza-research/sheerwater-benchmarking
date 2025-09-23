257c2c7996f8e63ece5ba1b120df944b3c53ea50
Joshua Adkins <josh@rhizaresearch.org>
2025-09-23 15:26:31 -0700

    try to import

--- infrastructure/terraform-config/import.sh
+++ infrastructure/terraform-config/import.sh
@@ -4,8 +4,9 @@ workspace=`terraform workspace show`
 
 if [ "$workspace" == "default" ]; then
     # this is production, so the benchmarking org is the main org (id 1)
-    terraform import grafana_organization.benchmarking 1
+    #terraform import grafana_organization.benchmarking 1
     #terraform import grafana_organization_preferences.light_preference_benchmarking 1
+    true
 else
     # this is ephemeral, so the benchmarking org is the main org (id 1)
     terraform import grafana_organization.benchmarking 1
